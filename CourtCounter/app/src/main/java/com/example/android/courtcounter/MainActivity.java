de07898d9bbf5438f5444a9c9bda1928434a9dd5
Logan Thomas <ltogan.2014@gmail.com>
2019-05-02 05:24:15 -0400

    Add Team B functionality

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -36,5 +45,20 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamA(View view) {
         scoreTeamA++;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
     }
 }
