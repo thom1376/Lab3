dd0008ec4fab923f392e07f9f94ab2e35c495b7c
Logan Thomas <ltogan.2014@gmail.com>
2019-05-02 01:50:28 -0400

    Add save and restore instance state

--- HelloCompat/app/src/main/java/com/example/hellocompat/MainActivity.java
+++ HelloCompat/app/src/main/java/com/example/hellocompat/MainActivity.java
@@ -13,13 +13,24 @@ public class MainActivity extends AppCompatActivity {
     private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
             "indigo", "blue", "light_blue", "cyan", "teal", "green",
             "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
-            "brown", "grey", "blue_grey", "black" };
+            "brown", "grey", "blue_grey", "black"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHelloTextView = findViewById(R.id.hello_textview);
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
     }
 
     public void changeColor(View view) {
