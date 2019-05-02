eec98dfa8853791e54e96ab31fb54357e92a4eba
Logan Thomas <ltogan.2014@gmail.com>
2019-05-02 03:33:31 -0400

    Update submitOrder method

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -26,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2*5);
+        displayPrice(quantity * 5);
     }
 
     /**
